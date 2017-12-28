d7bb75d7c7cdb2a48c15bf4ee85f67a63f618ff0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-28 19:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let deliveryId = 0
 
       customer() {
         return store.customers.find(function(customerId){
-          console.log(store.customers['id'])// === customerId 
+          console.log(store.customers['id'] === 5)// === customerId 
         })
       }
     }
