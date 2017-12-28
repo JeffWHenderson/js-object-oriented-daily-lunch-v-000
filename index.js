3a71bf0a13468aef36b88258f3235e5da90217e3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-28 19:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class Customer {
 
   deliveries() {
     return store.deliveries.filter(delivery => {
-      delivery.customerId === this.id
+      return delivery.customerId === this.id
     })
   }
 }
