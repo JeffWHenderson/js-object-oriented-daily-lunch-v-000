b6d1493126e2086927372a542aa478fe5dad1e20
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-28 18:56:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let store = []
+
+class Customer {
+  constructor(name) {
+    this.name = name
+  }
+}
+class Employer {
+  constructor {
+    
+  }
+}
 class Delivery {
-  
+  constructor {
+    
+  }
 }
