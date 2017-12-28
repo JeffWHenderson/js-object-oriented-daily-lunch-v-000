b1ff8bc5d53f5ebb43d9c0e4347377b5c31dc6d9
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-28 19:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let store = {customers: []}
 
 let mealId = 0
-
+let customerId = 0
 
 
 
 class Customer {
   constructor(name) {
+    this.id = ++customerId
     this.name = name
-
+    
     store.customers.push(this)
   }
 }
